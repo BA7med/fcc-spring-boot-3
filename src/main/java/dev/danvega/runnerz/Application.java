f02cb8a5076a9244bb941012588320b44b50e175
BA7med <ahmed.bakkari@digisystems.ma>
2024-08-07 11:22:00 +0100

    init

--- src/main/java/dev/danvega/runnerz/Application.java
+++ src/main/java/dev/danvega/runnerz/Application.java
@@ -1,6 +1,10 @@
 package dev.danvega.runnerz;
 
+import dev.danvega.runnerz.run.JdbcRunRepository;
+import dev.danvega.runnerz.run.Location;
+import dev.danvega.runnerz.run.Run;
 import dev.danvega.runnerz.user.UserHttpClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,6 +13,8 @@ import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class Application {
 
@@ -16,6 +22,14 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+
+//	@Bean
+//	CommandLineRunner runner (JdbcRunRepository runRepository) {
+//		return args -> {
+//			Run run = new Run(11, "Noon Run", LocalDateTime.of(2024, 2, 20, 6, 5), LocalDateTime.of(2024, 2, 20, 10, 27), 24, Location.INDOOR);
+//			runRepository.create(run);
+//		};
+//	}
 	@Bean
 	UserHttpClient userHttpClient() {
 	    RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
